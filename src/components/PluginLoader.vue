c63caeaaaac1e7b925f60bfc1b24ce68dba20a73
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2021-08-11 13:53:36 +0430

    Add transitions

--- src/components/PluginLoader.vue
+++ src/components/PluginLoader.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="row g-5">
+  <div class="row g-lg-5 g-4">
     <div
       v-for="(plugin, index) in plugins"
       :key="`${plugin.title}-${index}`"
       class="col-xl-4 col-md-6 col-12"
       v-show="isShowPlugin(tabName, nonSpaceFormat(plugin.title))"
     >
-      <PluginBox
-        v-if="isShowPlugin(tabName, nonSpaceFormat(plugin.title))"
-        :title="`${capitalize(plugin.title)}`"
-        :description="plugin.description"
-        :disabled="
-          isDisabledAllPlugins ||
-          selectedTabDate(tabName).disabled.includes(
-            nonSpaceFormat(plugin.title)
-          )
-        "
-        :is-active="
-          selectedTabDate(tabName).active.includes(nonSpaceFormat(plugin.title))
-        "
-        @update="onUpdatePlugin($event, nonSpaceFormat(plugin.title))"
-      />
+      <transition name="fade">
+        <PluginBox
+          v-if="isShowPlugin(tabName, nonSpaceFormat(plugin.title))"
+          :title="`${capitalize(plugin.title)}`"
+          :description="plugin.description"
+          :disabled="
+            isDisabledAllPlugins ||
+            selectedTabDate(tabName).disabled.includes(
+              nonSpaceFormat(plugin.title)
+            )
+          "
+          :is-active="
+            selectedTabDate(tabName).active.includes(
+              nonSpaceFormat(plugin.title)
+            )
+          "
+          @update="onUpdatePlugin($event, nonSpaceFormat(plugin.title))"
+        />
+      </transition>
     </div>
   </div>
 </template>
